57b7462a01b3913479f2e436e7e99d2f118972be
bharat shah <akashche72@gmail.com>
2021-10-10 18:58:57 +0400

    Footer has been added in the site

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -10,66 +10,72 @@ interface FooterProps {
 
 export const Footer: React.FC<FooterProps> = ({ }) => {
         return (<footer className="footer">
-                <div className="footer__top">
-                        <div className="item logo">
-                                <img src={Logo} alt="" />
-                        </div>
-                        <div className="item ready">
-                                Ready to get started?
-                        </div>
-                        <div className="item button">
-                                <button className="btn btn--primary">Get started</button>
-                        </div>
-                </div>
-
-                <div className="footer__main">
-                        <div className="item">
-                                <div className="row newsletter-txt">
-                                        Subscribe to our
-                                        newsletter
+                <div className="footer__container">
+                        <div className="footer__top">
+                                <div className="item logo">
+                                        <img src={Logo} alt="" />
                                 </div>
-                                <div className="row input">
-                                        <input type="text" className="input__subscribe" />
-                                        <button className="btn__subscript">
-                                                <img src={ArrowRight} alt="" />
-                                        </button>
+                                <div className="item ready">
+                                        Ready to get started?
+                                </div>
+                                <div className="item button">
+                                        <button className="btn btn--primary">Get started</button>
                                 </div>
                         </div>
-                        <div className="item services">
-                                <ul className="services__ul">
-                                        <li><a href="!#">Email Marketing</a></li>
-                                        <li><a href="!#">Email Marketing</a></li>
-                                        <li><a href="!#">Email Marketing</a></li>
-                                        <li><a href="!#">Email Marketing</a></li>
-                                        <li><a href="!#">Email Marketing</a></li>
-                                </ul>
+
+                        <div className="footer__main">
+                                <div className="item newsletter">
+                                        <div className="row newsletter-txt">
+                                                Subscribe to our
+                                                newsletter
+                                        </div>
+                                        <div className="row input">
+                                                <input placeholder = "Email address" type="text" className="input__subscribe" />
+                                                <button className="btn__subscript">
+                                                        <img src={ArrowRight} alt="" />
+                                                </button>
+                                        </div>
+                                </div>
+                                
+                                <div className="item services">
+                                        <ul className="services__ul">
+                                                <li><a href="!#">Email Marketing</a></li>
+                                                <li><a href="!#">Email Marketing</a></li>
+                                                <li><a href="!#">Email Marketing</a></li>
+                                                <li><a href="!#">Email Marketing</a></li>
+                                                <li><a href="!#">Email Marketing</a></li>
+                                        </ul>
+                                </div>
+                                <div className="item about">
+                                        <ul className="about__ul">
+                                                <li><a href="!#">Our Story</a></li>
+                                                <li><a href="!#">Our Story</a></li>
+                                                <li><a href="!#">Our Story</a></li>
+                                                <li><a href="!#">Our Story</a></li>
+                                        </ul>
+                                </div>
+                                <div className="item help">
+                                        <ul className="help__ul">
+                                                <li><a href="!#">FAQS</a></li>
+                                                <li><a href="!#">Concats Us</a></li>
+                                        </ul>
+                                </div>
                         </div>
-                        <div className="item about">
-                                <ul className="about__ul">
-                                        <li><a href="!#">Our Story</a></li>
-                                        <li><a href="!#">Our Story</a></li>
-                                        <li><a href="!#">Our Story</a></li>
-                                        <li><a href="!#">Our Story</a></li>
-                                </ul>
-                        </div>
-                        <div className="item help">
-                                <ul className="help__ul">
-                                        <li><a href="!#">FAQS</a></li>
-                                        <li><a href="!#">Concats Us</a></li>
-                                </ul>
-                        </div>
-                </div>
-                <div className="footer__bottom">
-                        <div className="item">
-                           Terms & Conditions
-                        </div>
-                        <div className="item">
-                        Privacy
+                        <div className="footer__bottom">
+                                <div className="item">
+                                        Terms & Conditions
+                                </div>
+                                <div className="item privacy">
+                                        Privacy
+                                </div>
+
+                                <div className="item social">
+                                        <img src= {Facebook} alt="" className="" />
+                                        <img src= {Twitter} alt="" className="" />
+                                        <img src= {Instagram} alt="" className="" />
+                                </div>
                         </div>
 
-                        <div className="item soical">
-
-                        </div>
                 </div>
         </footer>);
 }
