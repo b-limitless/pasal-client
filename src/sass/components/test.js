2448b08a44fd521abf9db82fd706e3c2675785dc
Bharat Shah <akashche72@gmail.com>
2021-12-29 09:26:30 +0400

    Pushing to remote from private rapo

---
+++ src/sass/components/test.js
@@ -0,0 +1,7 @@
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+console.log(randomColor);
+
+const rand = Math.floor( Math.random() * 10 ) + 0;
+console.log(rand)
+console.log(rand);
---
+++ test.js
@@ -0,0 +1,11 @@
+const reg = /^((http(s?)?):\/\/)?([wW]{3}\.)?[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/g;
+const string = 'https://www.speedydrive.ae/cache/promotions/wall2.jpeg';
+
+if(reg.test(string)) {
+   console.log('valsdsdfsdsffsdfsdfs%')
+} else {
+   console.log('not valid')
+}
+
+
+Marcus Andersen
